0a8a1dcded0abbd210b277c8280fadb038f22bf9
tyguy89 <121373027+tyguy89@users.noreply.github.com>
2025-12-12 16:10:37 -0600

    Delete JavaTargetTrainer directory

--- JavaTargetTrainer/src/main/java/com/example/TargetA4tjb404/SelectionBox.java
+++
@@ -1,9 +0,0 @@
-/*
-Tyler Boechler
- */
-
-package com.example.TargetA4tjb404;
-
-public class SelectionBox {
-
-}
--- JavaTargetTrainer/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.blobdemo2022 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.TargetA4tjb404 to javafx.fxml;
-    exports com.example.TargetA4tjb404;
-}
